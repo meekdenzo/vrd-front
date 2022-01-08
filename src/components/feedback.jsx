2d36da15cb9e64e6037e7bc00fcd4792b48d51bf
Parallels <meekdenzo@gmail.com>
2022-01-08 13:38:03 -0600

    code: components

---
+++ src/components/feedback.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function feedback(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+feedback.propTypes = {
+
+}
+
+export default feedback
+
---
+++ src/components/scan.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function scan(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+scan.propTypes = {
+
+}
+
+export default scan
+
