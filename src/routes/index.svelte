3531ca29c74190c07bc7f63f820f343101fbb7f4
Meek Denzo <meekdenzo@gmail.com>
2022-03-19 12:22:44 -0500

    add: response-area

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,13 @@
 <h1>VRD</h1>
 
 <body>
-	<div>
+	<div class="search-area">
 		<input type="text" placeholder="Enter regex...">
 		<button>Scan</button>
 	</div>
-	
+	<div class="response-area">
+		<p>This is where the response from server will be.</p>
+	</div>
 	<footer>A tool to test regex for DoS vulnerabilities.</footer>
 </body>
 
@@ -19,5 +21,13 @@
 	footer{
 		margin-top: auto;
 		text-align: center;
+	}
+	.search-area{
+		border-bottom: 1px solid gray;
+		padding-bottom: 10px;
+	}
+	.response-area{
+		/* border-color: rgb(7, 7, 7); */
+		/* border-style: solid; */
 	}
 </style>
